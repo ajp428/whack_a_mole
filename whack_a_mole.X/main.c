b7acfdb0cd05bf29c9e04ccee4bec3247c320624
Andrew <ajp428@gmail.com>
2020-03-26 16:32:14 -0500

    Commit

---
+++ whack_a_mole.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: User
+ *
+ * Created on March 26, 2020, 4:30 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
