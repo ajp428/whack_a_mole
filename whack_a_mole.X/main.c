79a67d8d4526b34043694de0c9e9d29776e14cc6
Andrew <ajp428@gmail.com>
2020-03-26 22:19:55 -0500

    Tested GPIO
    Got LED to turn on when button pressed, turn off when not pressed

--- whack_a_mole.X/main.c
+++ whack_a_mole.X/main.c
@@ -7,12 +7,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <xc.h>
+#include <stdint.h>
+#include <pic16f18446.h>
+#include <stdbool.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+void main(void) {
+    TRISCbits.TRISC7 = 0;   // Configure pin C7 at LED output
+    TRISBbits.TRISB7 = 1;   // Configure pin B7 as button input
+    ANSELBbits.ANSB7 = 0;   // Configure pin B7 for digital input
+    WPUBbits.WPUB7 = 1;     // Enable weak pull-up resistor for pin B7
+    bool led = false;
+    
+    while(1) {
+        if(PORTBbits.RB7 == 0) {
+            led = true;
+        } else {
+            led = false;
+        }
+        if(led) {
+            LATCbits.LATC7 = 1;
+        } else {
+            LATCbits.LATC7 = 0;
+        }
+    }
 }
 
