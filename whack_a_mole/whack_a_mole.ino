bfae715cc53599565c2772b740215ce59ee70a3e
Andrew <ajp428@gmail.com>
2020-04-08 03:08:14 -0500

    Defined TODOs

--- whack_a_mole/whack_a_mole.ino
+++ whack_a_mole/whack_a_mole.ino
@@ -1,9 +1,12 @@
 void setup() {
   //TODO define input pins
+  //Use pins A4-5, D0-5
 
   //TODO define output pins
+  //Use pins D6-13
 
   //TODO configure clock for 1 ms counter
+  
 }
 
 void loop() {
