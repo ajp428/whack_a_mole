1b24d9cc5f896a2ff876f2838b71969e5ae108c2
Andrew <ajp428@gmail.com>
2020-04-08 20:23:32 -0500

    Ezra's code

--- whack_a_mole/whack_a_mole.ino
+++ whack_a_mole/whack_a_mole.ino
@@ -1,9 +1,137 @@
+int PushB1 = A0;    // Pushbutton pins?
+int PushB2 = A1;
+int PushB3 = A2;
+int PushB4 = A3;
+int PushB5 = A4;
+int PushB6 = A5;
+int PBs1;           // Read variable for pins
+int PBs2;         
+int PBs3;
+int PBs4;
+int PBs5;
+int PBs6;
+
+int Bs;             // Button state
+
+int rNum;
+int wins=0;         
+int highScore=0;
+
+const int easy=1000;
+const int medium=500;
+const int hard=325;
+const int stupid=250;
+
+
+int ledPins[]={2,3,4,5,6,7,8,12};   // Why are there more than 6?
+int pinCount=8;          
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  pinMode(PushB1, INPUT);   // Setting input pins
+  pinMode(PushB2, INPUT);
+  pinMode(PushB3, INPUT);
+  pinMode(PushB4, INPUT);
+  pinMode(PushB5, INPUT);
+  pinMode(PushB6, INPUT);
+
+  pinMode(2, OUTPUT);       // Setting output pins
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+  rNum=random(0,5);
+  digitalWrite(ledPins[rNum], HIGH);
+  delay(easy);                          // Replace delay with timer comparison
+  Serial.println(rNum);                 
+  
+  PBs1 = digitalRead(PushB1);
+ 
+  PBs2 = digitalRead(PushB2);
+
+  PBs3 = digitalRead(PushB3);
+
+  PBs4 = digitalRead(PushB4);
+
+  PBs5 = digitalRead(PushB5);
+ 
+  PBs6 = digitalRead(PushB6);
+
+  Bs=0;                   // What does this do?
+  switch (PBs1) {         // Why not just if statements?
+    case 0:
+    Bs=1;
+    break;
+  } 
+  switch (PBs2) {
+    case 0:
+    Bs=2;
+    break;
+  }
+  switch (PBs3) {
+    case 0:
+    Bs=3;
+    break;
+  }
+  switch (PBs4) {
+    case 0:
+    Bs=4;
+    break;
+  }
+  switch (PBs5) {
+    case 0:
+    Bs=5;
+    break;
+  }
+  switch (PBs6) {
+    case 0:
+    Bs=6;
+    break;
+  }
+
+
+
+  digitalWrite(ledPins[rNum], LOW);
+  if (Bs-1==rNum){
+    wins++;
+    for (int k=0; k<=3; k++) {
+      Serial.println("Win!!!");
+      digitalWrite(ledPins[11], HIGH);    // I don't think we can access that element of the array
+      delay(50);
+      digitalWrite(ledPins[11], LOW);
+      delay(50);
+    }
+  }
+  else
+  {
+    if (wins>highScore) { 
+        highScore=wins;
+        wins=0;
+    }
+    for (int i=0; i<=3; i++) {   
+      //Serial.println("Fail");
+      digitalWrite(ledPins[12], HIGH);
+      delay(50);
+      digitalWrite(ledPins[12], LOW);
+      delay(50);
+    }
+    for (int w=0; w<highScore; w++) {
+        //digitalWrite(ledPins[8], HIGH);
+        digitalWrite(ledPins[12], HIGH);
+        delay(200);
+        //digitalWrite(ledPins[8], LOW);
+        digitalWrite(ledPins[12], LOW);
+        delay(200);
+    }  
+  }
+  //Serial.println(highScore);  
+  }
