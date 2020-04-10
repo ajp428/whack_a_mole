fcb6215cae7f358434a6171c5aa5079ee702eadc
Andrew <ajp428@gmail.com>
2020-04-10 02:33:15 -0500

    Added comments

--- whack_a_mole/whack_a_mole.ino
+++ whack_a_mole/whack_a_mole.ino
@@ -1,4 +1,16 @@
-int PushB1 = A0;    // Pushbutton pins?
+/*
+ * Whack-a-mole game
+ * 
+ * This code implements the game whack-a-mole. It includes all of the logic
+ * for initializing input and output pins on the Arduino Uno, and then 
+ * turning on LEDs at predetermined intervals and scanning for button input
+ * 
+ * Created April 2020
+ * 
+ * Authors: Daniel Andrews, Ezra Bailey-Kelly, Andrew phares
+ */
+
+int PushB1 = A0;    // Pushbutton pins
 int PushB2 = A1;
 int PushB3 = A2;
 int PushB4 = A3;
@@ -14,20 +26,20 @@ int PBs6;
 
 int Bs;             // Button state
 
-int rNum;
-int wins = 0;         
-int highScore = 0;
+int rNum;           // Random number
+int wins = 0;       // Number of successful times "whacking" the mole
+int highScore = 0;  
 
-const int easy = 1000;
+const int easy = 1000;    // Predetermined wait intervals
 const int medium = 500;
 const int hard = 325;
 const int stupid = 250;
 
 
-int ledPins[] = {2,3,4,5,6,7};   // Why are there more than 6?         
+int ledPins[] = {2,3,4,5,6,7};         
 
-uint32_t timeLastTurnedOn = 0;
-bool ledStates[] = {false, false, false, false, false, false};
+uint32_t timeLastTurnedOn = 0;      // Timer to keep track of when a currently on LED was turned on
+bool ledStates[] = {false, false, false, false, false, false}; // An array to keep track of which LED was on
 
 void setup() {
 
@@ -52,19 +64,19 @@ void setup() {
 
 void loop() {
 
-  if(millis() < timeLastTurnedOn) {
+  if(millis() < timeLastTurnedOn) { // Checking for timer overflow
     timeLastTurnedOn = 0;
     return;
   }
 
-  PBs1 = digitalRead(PushB1);
+  PBs1 = digitalRead(PushB1);       // Read to see if a button has been pressed
   PBs2 = digitalRead(PushB2);
   PBs3 = digitalRead(PushB3);
   PBs4 = digitalRead(PushB4);
   PBs5 = digitalRead(PushB5);
   PBs6 = digitalRead(PushB6);
 
-  Bs=0;
+  Bs=0; // Set the button state variable to whatever button may have been pressed, default 0
   switch (PBs1) {         
     case 0:
     Bs=1;
@@ -96,14 +108,14 @@ void loop() {
     break;
   }
 
-  if(numOn() == -1) {
+  if(numOn() == -1) {                   // If no LEDs, turn one on
     rNum = random(0,5);
     digitalWrite(ledPins[rNum], HIGH);
     ledStates[rNum] = true;
     timeLastTurnedOn = millis();
     Serial.println(rNum);
     Serial.println(millis());
-  } else {
+  } else {      // If an LED is on, see if enough time has elapsed
     if(millis() - timeLastTurnedOn > 1000) {
       digitalWrite(ledPins[rNum], LOW);
       ledStates[rNum] = false;
@@ -115,7 +127,7 @@ void loop() {
           digitalWrite(12, LOW);
           delay(50);
         }
-    } else {
+    } else {  // If the button was pressed, increment the score
       if (Bs - 1 == rNum) {
         wins++;
         Serial.println("Win!!!");
@@ -129,7 +141,7 @@ void loop() {
     }
   }
 
-  if (wins>highScore) { 
+  if (wins>highScore) { // Display the high score for Project 2
         highScore=wins;
         wins=0;
         for (int w=0; w<highScore; w++) {
@@ -144,7 +156,7 @@ void loop() {
   //Serial.println(highScore);  
 }
 
-int numOn() {
+int numOn() { // Custom function to determine which LED was on
   int i;
   for(i = 0; i < 6; i++) {
     if(ledStates[i]) {
